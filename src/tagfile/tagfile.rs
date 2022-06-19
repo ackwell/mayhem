6fd6d82df6e08580d93c78df5323420dd5861dd1
ackwell <saxon@ackwell.com.au>
2022-06-20 00:16:30 +1000

    Build up scaffold for tags

--- src/tagfile/tagfile.rs
+++ src/tagfile/tagfile.rs
@@ -2,7 +2,7 @@ use std::io::Read;
 
 use crate::error::{Error, Result};
 
-use super::common::read_u64;
+use super::{common::read_u64, i32::read_i32};
 
 // TODO: return type
 pub fn read(input: &mut impl Read) -> Result<()> {
@@ -12,5 +12,30 @@ pub fn read(input: &mut impl Read) -> Result<()> {
 		return Err(Error::Invalid(format!("Unexpected magic: {magic:#0x}")));
 	}
 
+	loop {
+		let tag = Tag::from(read_i32(input)?);
+		match tag {
+			Tag::Metadata => {
+				let version = read_i32(input)?;
+				println!("file version {version}");
+			}
+			other => todo!("Unhandled tag kind {other:?}"),
+		}
+	}
+
 	Ok(())
+}
+
+#[derive(Debug)]
+enum Tag {
+	Metadata,
+}
+
+impl From<i32> for Tag {
+	fn from(value: i32) -> Self {
+		match value {
+			1 => Self::Metadata,
+			other => todo!("Unhandled tag kind ID {other}."),
+		}
+	}
 }
