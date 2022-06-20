1f4834207851c3869f1e84572db125a789d56ccf
ackwell <saxon@ackwell.com.au>
2022-06-20 18:59:31 +1000

    Read instance definition index

--- src/tagfile/instance.rs
+++ src/tagfile/instance.rs
@@ -1,11 +1,22 @@
 use std::io::Read;
 
-use crate::error::Result;
+use crate::error::{Error, Result};
 
 use super::tagfile::Tagfile;
 
 impl<R: Read> Tagfile<R> {
 	pub fn read_instance(&mut self) -> Result<()> {
+		let definition_index = usize::try_from(self.read_i32()?).unwrap();
+		let definition = self
+			.definitions
+			.get(definition_index)
+			.and_then(|found| found.clone())
+			.ok_or_else(|| {
+				Error::Invalid(format!("Missing definition at index {definition_index}"))
+			})?;
+
+		println!("definition {definition:#?}");
+
 		todo!("instance")
 	}
 }
